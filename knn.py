9352bbacf41f08bed23571ca4ae640c359153184
Matheus <matheus281.1@hotmail.com>
2022-11-29 13:05:10 -0300

    adiciona comentarios no codigo, descrevendo o que determinadas linhas fazem

--- knn.py
+++ knn.py
@@ -31,13 +31,16 @@ knn.fit(X, Y)  # treina o knn com a diabetes700
 print("acuracia radada treino:", knn.score(X, Y))
 
 
+# importa a database69, que contem 68 novas linhas
 bd68 = pd.read_csv("diabetes68.csv")
 
+# ignora a coluna de resultado(Outcome), responsavel por diser se o paciente tem ou não tem Diabetes
 X_teste = np.array(bd68.drop('Outcome', axis=1))
-Y_teste = np.array(bd68.Outcome)
-loop = len(bd68)
-
-acerto = knn.predict(X_teste)
+Y_teste = np.array(bd68.Outcome)  # seta a coluna de resultado(Outcome)
 
 
+acerto = knn.predict(X_teste)  # predis se o passiente tem ou não tem Diabetes
+
+
+# mosta a acuracia do algoritmo, ou seja, a pontuação de eficiencia do algrtmo
 print("acuracia radada 2:", knn.score(X_teste, Y_teste))
